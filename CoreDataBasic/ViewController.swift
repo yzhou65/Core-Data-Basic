24f559dd282aaa2269872acaf6643d887f43a8aa
yzhou65 <andyzhou901@gmail.com>
2018-05-15 21:34:25 -0500

    Initial Commit

---
+++ CoreDataBasic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataBasic
+//
+//  Created by Yue Zhou on 5/15/18.
+//  Copyright © 2018 Yue Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
